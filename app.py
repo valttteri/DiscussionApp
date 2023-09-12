c16e29aa3d99f3d17aca06c281a821d9e6eb2259
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2023-09-12 15:55:53 +0300

    testing with postgresql database

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
-from flask import render_template, request
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+from os import getenv
 
 app = Flask(__name__)
+url =  getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = url
+db = SQLAlchemy(app)
 
 #etusivu
 @app.route('/', methods=['GET', 'POST'])
@@ -11,5 +17,18 @@ def index():
 #keskustelualueet
 @app.route('/forum', methods=['POST', 'GET'])
 def page2():
-    topic='topologia on kivaa'
-    return render_template('forum.html', topic=topic)
+    result = db.session.execute(text("SELECT content FROM messages"))
+    messages = result.fetchall()
+    return render_template('forum.html', messages=messages)
+
+@app.route("/new", methods=['GET', 'POST'])
+def new():
+    return render_template("new.html")
+
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    sql = text("INSERT INTO messages (content) VALUES (:content)")
+    db.session.execute(sql, {"content":content})
+    db.session.commit()
+    return redirect("/forum")
